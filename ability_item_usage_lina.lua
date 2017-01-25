fa166423f109d8c7c2a23e88f4394ee6179f7860
luisandia <luisandia@ucsp.edu.pe>
2017-01-25 17:03:44 -0500

    cuarto poder lina

--- ability_item_usage_lina.lua
+++ ability_item_usage_lina.lua
@@ -1,4 +1,5 @@
 
+deseo_lanzamiento_ulti = 0;
 deseo_lanzamiento_segunda = 0;
 deseo_lanzamiento_primera = 0;
 
@@ -8,18 +9,26 @@ function AbilityUsageThink()
 
 	local npcBot = GetBot();
 
-	print(npcBot:GetActiveMode())
+	--print(npcBot:GetActiveMode())
 	if ( npcBot:IsUsingAbility() ) then 
 		return 
 	end;
 	
 	habilidad_primera = npcBot:GetAbilityByName( "lina_dragon_slave" );
 	habilidad_segunda = npcBot:GetAbilityByName( "lina_light_strike_array" );
-	
+	habilidad_ulti = npcBot:GetAbilityByName( "lina_laguna_blade" );
 	
 	deseo_lanzamiento_primera, objetivo_localizacion = Tratar_dragon_slave();
 	deseo_lanzamiento_segunda, objetivo_localizacion_segunda = Tratar_Light_Strike_Array();
+	deseo_lanzamiento_ulti, objetivo_ulti = Tratar_Laguna_Blade();
 
+	if ( deseo_lanzamiento_ulti > deseo_lanzamiento_segunda and deseo_lanzamiento_ulti > deseo_lanzamiento_primera ) 
+	then
+		npcBot:Action_Chat("utilizo lina_laguna_blade(ULTI)",false);
+		npcBot:Action_UseAbilityOnEntity( habilidad_ulti, objetivo_ulti );
+		return;
+	end	
+	
 	if ( deseo_lanzamiento_primera > 0 ) 
 	then
 		npcBot:Action_Chat("utilizo lina_dragon_slave(PRIMERA)",false);
@@ -87,7 +96,7 @@ end
 
 
 
-
+-- funcion segundo poder
 function Tratar_Light_Strike_Array()
 
 	local npcBot = GetBot();
@@ -97,12 +106,12 @@ function Tratar_Light_Strike_Array()
 		return BOT_ACTION_DESIRE_NONE, 0;
 	end;
 
-	if ( castLBDesire > 0 ) 
+	if ( deseo_lanzamiento_ulti > 0 ) 
 	then
 		return BOT_ACTION_DESIRE_NONE, 0;
 	end
 
-	-- Get some of its values
+
 	local nRadius = habilidad_segunda:GetSpecialValueInt( "light_strike_array_aoe" );
 	local nCastRange = habilidad_segunda:GetCastRange();
 	local nDamage = habilidad_segunda:GetAbilityDamage();
@@ -126,3 +135,58 @@ function Tratar_Light_Strike_Array()
 
 	return BOT_ACTION_DESIRE_NONE, 0;
 end
+
+
+
+
+
+
+function CanCastLagunaBladeOnTarget( npcTarget )
+	return npcTarget:CanBeSeen() and npcTarget:IsHero() and ( GetBot():HasScepter() or not npcTarget:IsMagicImmune() ) and not npcTarget:IsInvulnerable();
+end
+
+function Tratar_Laguna_Blade()
+
+	local npcBot = GetBot();
+
+	
+	if ( not habilidad_ulti:IsFullyCastable() ) then 
+		return BOT_ACTION_DESIRE_NONE, 0;
+	end
+
+	local nCastRange = habilidad_ulti:GetCastRange();
+	local nDamage = habilidad_primera:GetSpecialValueInt( "damage" );
+	local eDamageType = npcBot:HasScepter() and DAMAGE_TYPE_PURE or DAMAGE_TYPE_MAGICAL;
+
+	local heroes_aliados_cerca = npcBot:GetNearbyHeroes( 1000, false, BOT_MODE_ATTACK );
+	if ( #heroes_aliados_cerca >= 2 ) 
+	then
+
+		local objetivo = nil;
+		local mayor_efecto = 0;
+		local heroes_enemigos_cerca = npcBot:GetNearbyHeroes( nCastRange, true, BOT_MODE_NONE );
+		--itero enemigos para obtener el que tenga menos vida
+		for _,npcEnemy in pairs( heroes_enemigos_cerca )
+		do
+			if ( CanCastLagunaBladeOnTarget( npcEnemy ) )
+			then
+				local nDamage = npcEnemy:GetEstimatedDamageToTarget( false, npcBot, 3.0, DAMAGE_TYPE_ALL );
+				if ( nDamage > mayor_efecto )
+				then
+					mayor_efecto = nDamage;
+					objetivo = npcEnemy;
+				end
+			end
+		end
+
+		if ( objetivo ~= nil )
+		then
+			return BOT_ACTION_DESIRE_HIGH, objetivo;
+		end
+	end
+
+	return BOT_ACTION_DESIRE_NONE, 0;
+
+end
+
+
