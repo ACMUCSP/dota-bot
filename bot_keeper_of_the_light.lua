190c58f394b9e590bc34162eaa513656e9594406
Aldo Culquicondor <alculquicondor@gmail.com>
2017-01-25 11:52:01 -0500

    patrolling towers

--- bot_keeper_of_the_light.lua
+++ bot_keeper_of_the_light.lua
@@ -1,32 +1,25 @@
-local positions = {Vector(0, 0), Vector(2000, 0), Vector(2000, 2000), Vector(0, 2000)};
-local pos_id = 1;
-local set = false;
+local positions = {
+    Vector(-104, -6244), Vector(4930, -6142),
+    Vector(-6195, 1787), Vector(-6143, -860),
+    Vector(-3540, -2791), Vector(-1660, -1516)
+};
+local pos_id = 0;
 
 
 function Think()
     local bot = GetBot();
-    if (not bot:IsAlive()) then
+    if (not bot:IsAlive() or DotaTime() < -88) then
         return;
     end
 
-    if (not set) then
-        local location = bot:GetLocation();
-        print('setting locations');
-        for i=1,4 do
-            positions[i] = location + positions[i];
-        end
-        set = true;
-        return;
-    end
-
-    if (DotaTime() < -88) then
-        return;
+    if (pos_id == 0) then
+        pos_id = 1;
+        bot:Action_MoveToLocation(positions[pos_id]);
     end
 
     if (GetUnitToLocationDistance(bot, positions[pos_id]) < 200) then
-        print('moving!');
         pos_id = pos_id + 1;
-        if (pos_id > 4) then
+        if (pos_id > #positions) then
             pos_id = 1;
         end
         bot:Action_MoveToLocation(positions[pos_id]);
